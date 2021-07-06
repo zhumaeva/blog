951c43421e53978c891dc2e0304a40277c23b46d
saule <zhumaeva.saule@gmail.com>
2021-07-06 22:04:41 +0600

    add admin, change post fields, settigs in setting, add get_photo

--- post/admin.py
+++ post/admin.py
@@ -1,3 +1,28 @@
 from django.contrib import admin
+from post.models import *
+from django.utils.safestring import mark_safe
 
+@admin.register(Tag)
+class TagAdmin(admin.ModelAdmin):
+    list_display = ('id', 'title')
+    prepopulated_fields = {'slug':('title',)}
 
+@admin.register(Category)
+class CategoryAdmin(admin.ModelAdmin):
+    list_display = ('id', 'title')
+    prepopulated_fields = {'slug':('title',)}
+
+@admin.register(Post)
+class PostAdmin(admin.ModelAdmin):
+    list_display = ('id', 'title', 'created_at', 'author', 'views', 'get_photo')
+    prepopulated_fields = {'slug': ('title',)}
+    fields = ('title', 'slug', 'author', 'category', 'content', 'tags', 'views', 'photo',)
+    list_filter = ('author', 'category', 'created_at')
+    readonly_fields = ('views','get_photo',)
+
+    def get_photo(self, obj):
+        if obj.photo:
+            return mark_safe(f'<img src="{obj.photo.url}" width="50">')
+        return '-'
+
+    get_photo.short_description = 'Фото'
--- post/models.py
+++ post/models.py
@@ -33,14 +33,17 @@ class Category(models.Model):
 
 class Post(models.Model):
 
-    ttitle = models.CharField(max_length=250, verbose_name='Заголовок')
+    title = models.CharField(max_length=250, verbose_name='Заголовок')
     content = models.TextField(verbose_name='Контент')
-    created_at = models.DateTimeField(auto_now_add=True, verbose_name='Дата создания')
-    author = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.PROTECT, verbose_name='Автор')
-    category = models.ForeignKey(Category, on_delete=models.CASCADE, verbose_name='Категория')
+    created_at = models.DateTimeField(auto_now_add=True, verbose_name='Опубликован')
+    author = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.PROTECT, verbose_name='Автор', 
+                                related_name='posts')
+    category = models.ForeignKey(Category, on_delete=models.CASCADE, verbose_name='Категория', 
+                                related_name='posts')
     tags = models.ManyToManyField(Tag, verbose_name='Тэги')
     slug = models.SlugField(verbose_name='Слаг')
-    views = models.PositiveIntegerField(default=0)
+    views = models.PositiveIntegerField(default=0, verbose_name='Количество просмотров')
+    photo = models.ImageField(upload_to='posts/%Y/%m/%d/',verbose_name = 'Фото', null=True, blank=True)
 
     def __str__(self):
         return self.title
